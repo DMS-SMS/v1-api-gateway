63616925436acf67a316caf8dc10bc7a101577c5
지농 <jinhong0719@naver.com>
2020-10-20 22:34:52 +0900

    [ADD] <tool/consul/agent.go> Create consul agent interface

--- tool/consul/agent.go
+++ tool/consul/agent.go
@@ -1,1 +1,9 @@
 package consul
+
+import "github.com/micro/go-micro/v2/registry"
+
+type Agent interface {
+	GetNextServiceNode(serviceID string) (*registry.Node, error)
+	SetFailTTLHealth(checkID, note string) error
+	SetPassTTLHealth(checkID, note string) error
+}
