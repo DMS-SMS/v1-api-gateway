099a942528cf1657bc73653817a18d923aa56c89
지농 <jinhong0719@naver.com>
2020-10-20 22:27:22 +0900

    [ADD] <handler/default.go> Create default handler struct & Add field

--- handler/default.go
+++ handler/default.go
@@ -1,8 +1,36 @@
 package handler
 
 import (
+	authproto "gateway/proto/golang/auth"
+	clubproto "gateway/proto/golang/club"
+	"github.com/eapache/go-resiliency/breaker"
+	"github.com/go-playground/validator/v10"
+	"github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
+
+type _default struct {
+	authService interface {
+		authproto.AuthAdminService
+		authproto.AuthStudentService
+		authproto.AuthTeacherService
+		authproto.AuthParentService
+	}
+	clubService interface {
+		clubproto.ClubAdminService
+		clubproto.ClubStudentService
+		clubproto.ClubLeaderService
+	}
+	logger     *logrus.Logger
+	tracer     opentracing.Tracer
+	validate   *validator.Validate
+	breakers   map[string]*breaker.Breaker
+	BreakerCfg BreakerConfig
+	mutex      sync.Mutex
+	// consul agent 인터페이스 추가 예정
+}
 
 type BreakerConfig struct {
 	ErrorThreshold   int
