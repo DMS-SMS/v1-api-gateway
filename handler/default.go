18434bc844d8c2c5af6b74be7bcc4cdb8bc73526
지농 <jinhong0719@naver.com>
2020-10-20 23:08:47 +0900

    [ADD] <handler/default.go> Add function to return closure setting default field

--- handler/default.go
+++ handler/default.go
@@ -27,8 +27,8 @@ type _default struct {
 	consulAgent consul.Agent
 	logger      *logrus.Logger
 	tracer      opentracing.Tracer
-	breakers    map[string]*breaker.Breaker
 	validate    *validator.Validate
+	breakers    map[string]*breaker.Breaker
 	mutex       sync.Mutex
 	BreakerCfg  BreakerConfig
 }
@@ -41,7 +41,6 @@ type BreakerConfig struct {
 
 func Default(setters ...FieldSetter) (h *_default) {
 	h = new(_default)
-
 	for _, setter := range setters {
 		setter(h)
 	}
@@ -51,8 +50,55 @@ func Default(setters ...FieldSetter) (h *_default) {
 		SuccessThreshold: 3,
 		Timeout:          time.Minute,
 	}
+	h.mutex = sync.Mutex{}
+	h.breakers = map[string]*breaker.Breaker{}
 
 	return
 }
 
 type FieldSetter func(*_default)
+
+func AuthService(authService struct {
+	authproto.AuthAdminService
+	authproto.AuthStudentService
+	authproto.AuthTeacherService
+	authproto.AuthParentService
+}) FieldSetter {
+	return func(h *_default) {
+		h.authService = authService
+	}
+}
+
+func ClubService(clubService struct {
+	clubproto.ClubAdminService
+	clubproto.ClubStudentService
+	clubproto.ClubLeaderService
+}) FieldSetter {
+	return func(h *_default) {
+		h.clubService = clubService
+	}
+}
+
+func ConsulAgent(consulAgent consul.Agent) FieldSetter {
+	return func(h *_default) {
+		h.consulAgent = consulAgent
+	}
+}
+
+func Logger(logger *logrus.Logger) FieldSetter {
+	return func(h *_default) {
+		h.logger = logger
+	}
+}
+
+func Tracer(tracer opentracing.Tracer) FieldSetter {
+	return func(h *_default) {
+		h.tracer = tracer
+	}
+}
+
+func Validate(validate *validator.Validate) FieldSetter {
+	return func(h *_default) {
+		h.validate = validate
+	}
+}
