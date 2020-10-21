179bb79286105a594e5fcb0021229029f9e82485
지농 <jinhong0719@naver.com>
2020-10-21 16:45:03 +0900

    [ADD] <handler/middleware/dosDetector.go> add dosDetector struct and initializer

--- handler/middleware/dosDetector.go
+++ handler/middleware/dosDetector.go
@@ -1,1 +1,17 @@
 package middleware
+
+import "sync"
+
+type dosDetector struct {
+	limitTable map[string]*uint32
+	rejected   map[string]bool
+	mutex      sync.Mutex
+}
+
+func DosDetector() *dosDetector {
+	return &dosDetector{
+		limitTable: make(map[string]*uint32),
+		rejected:   make(map[string]bool),
+		mutex:      sync.Mutex{},
+	}
+}
