98f0d0692b692eedbf4c818f7ad86de16716bc36
지농 <jinhong0719@naver.com>
2020-10-21 22:57:27 +0900

    [ADD] <handler/deafult_auth_admin.go> Add get request log entry & handling logic

--- handler/default_auth_admin.go
+++ handler/default_auth_admin.go
@@ -1,1 +1,31 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func (h *_default) CreateNewStudent(c *gin.Context) {
+	reqID := c.GetHeader("X-Request-Id")
+
+	// 로그에 필요한 정보
+	// path, method, client_ip, X-Request-Id, header -> 미들웨어에서 처리
+	// request, response, status, code, message -> 핸들러에서 처치
+
+	inAdvanceEntry, ok := c.Get("RequestLogEntry")
+	entry, ok := inAdvanceEntry.(*logrus.Entry)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "unable to get request log entry from middleware",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "succeed to create new club",
+	})
+	return
+}
