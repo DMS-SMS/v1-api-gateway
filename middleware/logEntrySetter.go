d7a7dece058f5ab727a286c641efcdf961b8d00c
지농 <jinhong0719@naver.com>
2020-10-21 22:07:45 +0900

    [ADD] <middleware/logEntrySetter.go> Add LogEntrySetter struct & initializer

--- middleware/logEntrySetter.go
+++ middleware/logEntrySetter.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type logEntrySetter struct {
+	logger *logrus.Logger
+}
+
+func LogEntrySetter(l *logrus.Logger) gin.HandlerFunc {
+	return (&logEntrySetter{
+		logger: l,
+	}).setLogEntry
+}
+
+// Log entry에 path, method, client_ip, X-Request-Id, header 정보 저장
+func (l *logEntrySetter) setLogEntry(c *gin.Context) {
+	headerBytes, _ := json.Marshal(c.Request.Header)
+
+	entry := l.logger.WithFields(logrus.Fields{
+		"path":         c.Request.URL.Path,
+		"method":       c.Request.Method,
+		"client_ip":    c.ClientIP(),
+		"X-Request-Id": c.GetHeader("X-Request-Id"),
+		"header":       string(headerBytes),
+	})
+	c.Set("RequestLogEntry", entry)
+}
