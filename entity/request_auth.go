9454b645334aae5e6604f6ae64482037c7b6085a
지농 <jinhong0719@naver.com>
2020-10-22 17:02:25 +0900

    [MODIFY] <entity/request_auth.go> Change length -> len in validate tag

--- entity/request_auth.go
+++ entity/request_auth.go
@@ -4,7 +4,7 @@ package entity
 type CreateNewStudentRequest struct {
 	StudentID   string `json:"student_id" validate:"required,min=4,max=16"`
 	StudentPW   string `json:"student_pw" validate:"required,min=4,max=16"`
-	ParentUUID  string `json:"parent_uuid" validate:"required,uuid=parent,length=19"`
+	ParentUUID  string `json:"parent_uuid" validate:"required,uuid=parent,len=19"`
 	Grade       int    `json:"grade" validate:"required,intRange=1~3"`
 	Group       int    `json:"group" validate:"required,intRange=1~4"`
 	Number      int    `json:"number" validate:"required,intRange=1~21"`
