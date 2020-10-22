7a671779148a5f8cbe26cf80c8bef1603e249b32
지농 <jinhong0719@naver.com>
2020-10-22 12:04:01 +0900

    [ADD] <tool/jwt/generate.go> Add function to generate jwt string with claims

--- tool/jwt/generate.go
+++ tool/jwt/generate.go
@@ -1,1 +1,21 @@
 package jwt
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func GenerateStringWithClaims(claims jwt.Claims, method jwt.SigningMethod) (ss string, err error) {
+	keyPath, err := filepath.Abs("tool/jwt/jwt_key.priv")
+	if err != nil {
+		return
+	}
+	jwtKey, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		return
+	}
+
+	ss, err = jwt.NewWithClaims(method, claims).SignedString(jwtKey)
+	return
+}
