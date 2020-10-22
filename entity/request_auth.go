96e29125a803ef25bc5ac8a6936f9b30ab950c8a
지농 <jinhong0719@naver.com>
2020-10-22 16:54:35 +0900

    [ADD] <entity/request_auth.go> Add CreateNewStudentRequest for POST /v1/clubs

--- entity/request_auth.go
+++ entity/request_auth.go
@@ -1,1 +1,13 @@
 package entity
+
+// request entity for POST /v1/clubs
+type CreateNewStudentRequest struct {
+	StudentID   string `json:"student_id" validate:"required,min=4,max=16"`
+	StudentPW   string `json:"student_pw" validate:"required,min=4,max=16"`
+	ParentUUID  string `json:"parent_uuid" validate:"required,uuid=parent,length=19"`
+	Grade       int    `json:"grade" validate:"required,intRange=1~3"`
+	Group       int    `json:"group" validate:"required,intRange=1~4"`
+	Number      int    `json:"number" validate:"required,intRange=1~21"`
+	Name        string `json:"name" validate:"required,korean,min=2,max=4"`
+	PhoneNumber string `json:"phone_number" validate:"phone_number,len=11"`
+}
