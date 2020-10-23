767fb356ad984f111d6f53309cb19b08efa789f8
지농 <jinhong0719@naver.com>
2020-10-23 19:29:20 +0900

    [ADD] <tool/consul/agent/default.go> Add declaring default struct

--- tool/consul/agent/default.go
+++ tool/consul/agent/default.go
@@ -1,1 +1,14 @@
 package agent
+
+import (
+	"github.com/hashicorp/consul/api"
+	"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/registry"
+)
+
+type _default struct {
+	Strategy selector.Strategy
+	client   *api.Client
+	next     selector.Next
+	nodes    []*registry.Node
+}
