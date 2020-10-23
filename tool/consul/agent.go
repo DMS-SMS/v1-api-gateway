b6df3278141af7cd87a32ea56c72194a76acec4e
지농 <jinhong0719@naver.com>
2020-10-23 20:06:24 +0900

    [MODIFY] <tool/consul/agent.go> Change some method name of Agent interface

--- tool/consul/agent.go
+++ tool/consul/agent.go
@@ -4,6 +4,6 @@ import "github.com/micro/go-micro/v2/registry"
 
 type Agent interface {
 	GetNextServiceNode(serviceID string) (*registry.Node, error)
-	SetFailTTLHealth(checkID, note string) error
-	SetPassTTLHealth(checkID, note string) error
+	FailTTLHealth(checkID, note string) error
+	PassTTLHealth(checkID, note string) error
 }
