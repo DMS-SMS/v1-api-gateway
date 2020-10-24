4db2d61d5f1aeec7d29ecf61f8ac0f174d6ef9a8
지농 <jinhong0719@naver.com>
2020-10-24 11:38:20 +0900

    [ADD] <entity/validator/regexes.go> Add const string about regular expression

--- entity/validator/regexes.go
+++ entity/validator/regexes.go
@@ -1,1 +1,13 @@
 package validator
+
+const (
+	adminUUIDRegexString = "^admin-\\d{12}"
+	studentUUIDRegexString = "^student-\\d{12}"
+	teacherUUIDRegexString = "^teacher-\\d{12}"
+	parentUUIDRegexString = "^parent-\\d{12}"
+	clubUUIDRegexString = "^club-\\d{12}"
+	outingUUIDRegexString = "^outing-\\d{12}"
+	announcementUUIDRegexString = "^announcement-\\d{12}"
+	recruitmentUUIDRegexString = "^recruitment-\\d{12}"
+	timeRegexString = "\\d{4}-\\d{2}-\\d{2}"
+)
