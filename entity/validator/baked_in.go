47dea3ec0d55e729296957847f31cacbed2f476e
지농 <jinhong0719@naver.com>
2020-10-24 11:42:39 +0900

    [ADD] <entity/validator/baked_in.go> Add function to check if field is valid uuid

--- entity/validator/baked_in.go
+++ entity/validator/baked_in.go
@@ -1,1 +1,25 @@
 package validator
+
+import "github.com/go-playground/validator/v10"
+
+func isValidateUUID(fl validator.FieldLevel) bool {
+	switch fl.Param() {
+	case "admin":
+		return adminUUIDRegex.MatchString(fl.Field().String())
+	case "student":
+		return studentUUIDRegex.MatchString(fl.Field().String())
+	case "teacher":
+		return teacherUUIDRegex.MatchString(fl.Field().String())
+	case "parent":
+		return parentUUIDRegex.MatchString(fl.Field().String())
+	case "club":
+		return clubUUIDRegex.MatchString(fl.Field().String())
+	case "outing":
+		return outingUUIDRegex.MatchString(fl.Field().String())
+	case "announcement":
+		return announcementUUIDRegex.MatchString(fl.Field().String())
+	case "recruitment":
+		return recruitmentUUIDRegex.MatchString(fl.Field().String())
+	}
+	return false
+}
