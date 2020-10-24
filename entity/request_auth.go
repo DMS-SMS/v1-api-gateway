1a85bfa872b85a379e16697a6d05d52d270d2efb
지농 <jinhong0719@naver.com>
2020-10-24 11:11:18 +0900

    [ADD] <entity/request_auth.go> Add method to generate request for gRPC call

--- entity/request_auth.go
+++ entity/request_auth.go
@@ -1,5 +1,9 @@
 package entity
 
+import (
+	authproto "gateway/proto/golang/auth"
+)
+
 // request entity of POST /v1/clubs
 type CreateNewStudentRequest struct {
 	StudentID     string `json:"student_id" validate:"required,min=4,max=16"`
@@ -10,4 +14,17 @@ type CreateNewStudentRequest struct {
 	StudentNumber int    `json:"number" validate:"required,intRange=1~21"`
 	Name          string `json:"name" validate:"required,korean,min=2,max=4"`
 	PhoneNumber   string `json:"phone_number" validate:"phone_number,len=11"`
+}
+
+func (from CreateNewStudentRequest) GenerateGRPCRequest() (to *authproto.CreateNewStudentRequest) {
+	to = new(authproto.CreateNewStudentRequest)
+	to.StudentID = from.StudentID
+	to.StudentPW = from.StudentPW
+	to.ParentUUID = from.ParentUUID
+	to.Grade = uint32(from.Grade)
+	to.Group = uint32(from.Group)
+	to.StudentNumber = uint32(from.StudentNumber)
+	to.Name = from.Name
+	to.PhoneNumber = from.PhoneNumber
+	return
 }
