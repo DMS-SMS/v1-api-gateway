9ddc649cae98125d14bf4a4654d93f3980d69a01
지농 <jinhong0719@naver.com>
2020-10-24 14:25:54 +0900

    [MODIFY] <entity/request_auth.go> Change validate tag name (intRange -> int_range)

--- entity/request_auth.go
+++ entity/request_auth.go
@@ -9,9 +9,9 @@ type CreateNewStudentRequest struct {
 	StudentID     string `json:"student_id" validate:"required,min=4,max=16"`
 	StudentPW     string `json:"student_pw" validate:"required,min=4,max=16"`
 	ParentUUID    string `json:"parent_uuid" validate:"required,uuid=parent,len=19"`
-	Grade         int    `json:"grade" validate:"required,intRange=1~3"`
-	Group         int    `json:"group" validate:"required,intRange=1~4"`
-	StudentNumber int    `json:"number" validate:"required,intRange=1~21"`
+	Grade         int    `json:"grade" validate:"required,int_range=1~3"`
+	Group         int    `json:"group" validate:"required,int_range=1~4"`
+	StudentNumber int    `json:"number" validate:"required,int_range=1~21"`
 	Name          string `json:"name" validate:"required,korean,min=2,max=4"`
 	PhoneNumber   string `json:"phone_number" validate:"phone_number,len=11"`
 }
