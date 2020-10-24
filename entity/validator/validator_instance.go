e45757ea39df067dcd8ab66f90baa182eb49571d
지농 <jinhong0719@naver.com>
2020-10-24 11:59:22 +0900

    [ADD] <entity/validator/validator_instance.go> Add global validator & init function

--- entity/validator/validator_instance.go
+++ entity/validator/validator_instance.go
@@ -1,1 +1,18 @@
 package validator
+
+import (
+	"github.com/go-playground/validator/v10"
+	"log"
+)
+
+var entityValidator *validator.Validate
+
+func init() {
+	entityValidator = validator.New()
+
+	if err := entityValidator.RegisterValidation("uuid", isValidateUUID); err == nil { log.Fatal(err) } // 문자열 전용
+	if err := entityValidator.RegisterValidation("int_range", isWithinIntRange); err == nil { log.Fatal(err) } // 정수 전용
+	if err := entityValidator.RegisterValidation("korean", isKoreanString); err == nil { log.Fatal(err) } // 문자열 전용
+	if err := entityValidator.RegisterValidation("phone_number", isPhoneNumber); err == nil { log.Fatal(err) } // 문자열 전용
+	if err := entityValidator.RegisterValidation("time", isTime); err == nil { log.Fatal(err) } // 문자열 전용
+}
