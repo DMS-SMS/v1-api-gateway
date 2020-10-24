959be1b6d6c3886583ffd8719c5395ecb2f7b308
지농 <jinhong0719@naver.com>
2020-10-24 10:51:29 +0900

    [ADD] <handler/default_auth_admin.go> Add setting log fields in top span

--- handler/default_auth_admin.go
+++ handler/default_auth_admin.go
@@ -5,24 +5,16 @@ import (
 	"gateway/entity"
 	"gateway/tool/jwt"
 	topic "gateway/utils/topic/golang"
+	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
-	//code "gateway/utils/code/golang"
-	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func (h *_default) CreateNewStudent(c *gin.Context) {
-	//reqID := c.GetHeader("X-Request-Id")
-
-	// 로그에 필요한 정보
-	// path, method, client_ip, X-Request-Id, header -> 미들웨어에서 처리
-	// request, response, status, code, message -> 핸들러에서 처치
-
 	reqID := c.GetHeader("X-Request-Id")
 	topSpan := h.tracer.StartSpan(c.Request.URL.Path).SetTag("X-Request-Id", reqID)
-	defer topSpan.Finish()
 
 	inAdvanceEntry, ok := c.Get("RequestLogEntry")
 	entry, ok := inAdvanceEntry.(*logrus.Entry)
@@ -30,6 +22,8 @@ func (h *_default) CreateNewStudent(c *gin.Context) {
 		msg := "unable to get request log entry from middleware"
 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "code": 0, "message": msg})
 		entry.WithFields(logrus.Fields{"status": http.StatusInternalServerError, "code": 0, "message": msg}).Warn()
+		topSpan.LogFields(log.Int("status", http.StatusInternalServerError), log.Int("code", 0), log.String("message", msg))
+		topSpan.Finish()
 		return
 	}
 
@@ -40,24 +34,26 @@ func (h *_default) CreateNewStudent(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusUnauthorized, gin.H{"status": http.StatusUnauthorized, "code": _code, "message": msg})
 		entry.WithFields(logrus.Fields{"status": http.StatusUnauthorized, "code": _code, "message": msg}).Info()
+		topSpan.LogFields(log.Int("status", http.StatusUnauthorized), log.Int("code", _code), log.String("message", msg))
+		topSpan.Finish()
 		return
 	}
 
 	// logic handling BadRequest
-	var req entity.CreateNewStudentRequest
-	if ok, _code, msg := h.checkIfValidRequest(c, &req); ok {
+	var receivedReq entity.CreateNewStudentRequest
+	if ok, _code, msg := h.checkIfValidRequest(c, &receivedReq); ok {
 	} else {
-		reqBytes, _ := json.Marshal(req)
+		reqBytes, _ := json.Marshal(receivedReq)
 		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "code": _code, "message": msg})
 		entry.WithFields(logrus.Fields{"status": http.StatusBadRequest, "code": _code, "message": msg, "request": string(reqBytes)}).Info()
+		topSpan.LogFields(log.Int("status", http.StatusBadRequest), log.Int("code", _code), log.String("message", msg))
+		topSpan.Finish()
 		return
 	}
 
 	consulSpan := h.tracer.StartSpan("GetNextServiceNode", opentracing.ChildOf(topSpan.Context()))
 	selectedNode, err := h.consulAgent.GetNextServiceNode(topic.AuthServiceName)
-	if err == nil {
-		consulSpan.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedNode", *selectedNode))
-	}
+	if err == nil { consulSpan.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedNode", *selectedNode)) }
 	consulSpan.LogFields(log.Error(err))
 	consulSpan.Finish()
 
