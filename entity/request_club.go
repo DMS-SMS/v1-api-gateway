0a3333df5c825dc8f907c4c81fcff25c54e7f9e5
지농 <jinhong0719@naver.com>
2020-10-25 21:54:33 +0900

    [ADD] <entity/request_club.go> Add request entity for CreateNewClub

--- entity/request_club.go
+++ entity/request_club.go
@@ -1,1 +1,36 @@
 package entity
+
+import (
+	clubproto "gateway/proto/golang/club"
+	"mime/multipart"
+	"strconv"
+	"strings"
+)
+
+// request entity of POST /v1/clubs
+type CreateNewClubRequest struct {
+	Name        string `form:"name" validate:"required,max=30"`
+	LeaderUUID  string `form:"leader_uuid" validate:"required,uuid=student,len=20"`
+	MemberUUIDs string `form:"member_uuids" validate:"required"`
+	Field       string `form:"field" validate:"required,max=20"`
+	Location    string `form:"location" validate:"required,max=20"`
+	Floor       int    `form:"floor" validate:"required,int_range=1~5"`
+	Logo        *multipart.FileHeader `form:"logo" validate:"required"`
+}
+
+func (from CreateNewClubRequest) GenerateGRPCRequest() (to *clubproto.CreateNewClubRequest) {
+	to = new(clubproto.CreateNewClubRequest)
+	to.Name = from.Name
+	to.LeaderUUID = from.LeaderUUID
+	to.MemberUUIDs = strings.Split(from.MemberUUIDs, "|")
+	to.Field = from.Field
+	to.Location = from.Location
+	to.Floor = strconv.Itoa(from.Floor)
+
+	to.Logo = make([]byte, from.Logo.Size)
+	file, _ := from.Logo.Open()
+	defer func() { _ = file.Close() }()
+	_, _ = file.Read(to.Logo)
+
+	return
+}
