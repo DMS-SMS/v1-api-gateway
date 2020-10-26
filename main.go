fd5926c916b96678aace44312e293e57c6f9f893
지농 <jinhong0719@naver.com>
2020-10-26 22:14:39 +0900

    [ADD] <main.go> Add creating dependency object for http handler

--- main.go
+++ main.go
@@ -1,5 +1,85 @@
 package main
 
-func main() {
+import (
+	"gateway/entity/validator"
+	"gateway/handler"
+	authproto "gateway/proto/golang/auth"
+	clubproto "gateway/proto/golang/club"
+	consulagent "gateway/tool/consul/agent"
+	topic "gateway/utils/topic/golang"
+	"github.com/hashicorp/consul/api"
+	"github.com/micro/go-micro/v2/client"
+	grpccli "github.com/micro/go-micro/v2/client/grpc"
+	"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/transport/grpc"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"log"
+	"os"
+)
 
+func main() {
+	// create consul connection
+	consulCfg := api.DefaultConfig()
+	consulCfg.Address = os.Getenv("CONSUL_ADDRESS")
+	if consulCfg.Address == "" {
+		log.Fatal("please set CONSUL_ADDRESS in environment variables")
+	}
+	consul, err := api.NewClient(consulCfg)
+	if err != nil {
+		log.Fatalf("unable to connect consul agent, err: %v", err)
+	}
+	consulAgent := consulagent.Default(
+		consulagent.Client(consul),
+		consulagent.Strategy(selector.RoundRobin),
+	)
+
+	// create jaeger connection
+	jaegerAddr := os.Getenv("JAEGER_ADDRESS")
+	if jaegerAddr == "" {
+		log.Fatal("please set JAEGER_ADDRESS in environment variables")
+	}
+	apiTracer, closer, err := jaegercfg.Configuration{
+		ServiceName: "DMS.SMS.v1.api.gateway", // add const in topic
+		Reporter: &jaegercfg.ReporterConfig{LogSpans: true, LocalAgentHostPort: jaegerAddr},
+		Sampler: &jaegercfg.SamplerConfig{Type: jaeger.SamplerTypeConst, Param: 1},
+	}.NewTracer()
+	if err != nil {
+		log.Fatalf("error while creating new tracer for service, err: %v", err)
+	}
+	defer func() {
+		_ = closer.Close()
+	}()
+
+	// gRPC service client
+	gRPCCli := grpccli.NewClient(client.Transport(grpc.NewTransport()))
+	authSrvCli := struct {
+		authproto.AuthAdminService
+		authproto.AuthStudentService
+		authproto.AuthTeacherService
+		authproto.AuthParentService
+	}{
+		AuthAdminService:   authproto.NewAuthAdminService(topic.AuthServiceName, gRPCCli),
+		AuthStudentService: authproto.NewAuthStudentService(topic.AuthServiceName, gRPCCli),
+		AuthTeacherService: authproto.NewAuthTeacherService(topic.AuthServiceName, gRPCCli),
+		AuthParentService:  authproto.NewAuthParentService(topic.AuthServiceName, gRPCCli),
+	}
+	clubSrvCli := struct {
+		clubproto.ClubAdminService
+		clubproto.ClubStudentService
+		clubproto.ClubLeaderService
+	}{
+		ClubAdminService:   clubproto.NewClubAdminService(topic.ClubServiceName, gRPCCli),
+		ClubStudentService: clubproto.NewClubStudentService(topic.ClubServiceName, gRPCCli),
+		ClubLeaderService:  clubproto.NewClubLeaderService(topic.ClubServiceName, gRPCCli),
+	}
+
+	// create http request handler
+	httpHandler := handler.Default(
+		handler.ConsulAgent(consulAgent),
+		handler.Validate(validator.New()),
+		handler.Tracer(apiTracer),
+		handler.AuthService(authSrvCli),
+		handler.ClubService(clubSrvCli),
+	)
 }
