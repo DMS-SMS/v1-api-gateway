f9164b2f4c013557406faa25be9d97900a63ef90
지농 <jinhong0719@naver.com>
2020-10-26 22:15:49 +0900

    [ADD] <main.go> Add routing auth & club service api for api gateway

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"gateway/entity/validator"
 	"gateway/handler"
+	"gateway/middleware"
 	authproto "gateway/proto/golang/auth"
 	clubproto "gateway/proto/golang/club"
 	consulagent "gateway/tool/consul/agent"
 	topic "gateway/utils/topic/golang"
+	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2/client"
 	grpccli "github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/transport/grpc"
+	"github.com/sirupsen/logrus"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"log"
@@ -82,4 +85,60 @@ func main() {
 		handler.AuthService(authSrvCli),
 		handler.ClubService(clubSrvCli),
 	)
+
+	router := gin.Default()
+	router.Use(middleware.DosDetector(), middleware.Correlator(), middleware.LogEntrySetter(logrus.New()))
+
+	// auth service api for admin
+	router.POST("/v1/students", httpHandler.CreateNewStudent)
+	router.POST("/v1/teachers", httpHandler.CreateNewTeacher)
+	router.POST("/v1/parents", httpHandler.CreateNewParent)
+	router.POST("/v1/login/admin", httpHandler.LoginAdminAuth)
+
+	// auth service api for student
+	router.POST("/v1/login/student", httpHandler.LoginStudentAuth)
+	router.PUT("/v1/students/uuid/:student_uuid/password", httpHandler.ChangeStudentPW)
+	router.GET("/v1/students/uuid/:student_uuid", httpHandler.GetStudentInformWithUUID)
+	router.GET("/v1/student-uuids", httpHandler.GetStudentUUIDsWithInform)
+	router.GET("/v1/students", httpHandler.GetStudentInformsWithUUIDs)
+
+	// auth service api for teacher
+	router.POST("/v1/login/teacher", httpHandler.LoginTeacherAuth)
+	router.PUT("/v1/teachers/uuid/:teacher_uuid/password", httpHandler.ChangeTeacherPW)
+	router.GET("/v1/teachers/uuid/:teacher_uuid", httpHandler.GetTeacherInformWithUUID)
+	router.GET("/v1/teacher-uuids", httpHandler.GetTeacherUUIDsWithInform)
+
+	// auth service api for parent
+	router.POST("/v1/login/parent", httpHandler.LoginParentAuth)
+	router.PUT("/v1/parents/uuid/:parent_uuid/password", httpHandler.ChangeParentPW)
+	router.GET("/v1/parents/uuid/:parent_uuid", httpHandler.GetParentInformWithUUID)
+	router.GET("/v1/parent-uuids", httpHandler.GetParentUUIDsWithInform)
+
+	// club service api for admin
+	router.POST("/v1/clubs", httpHandler.CreateNewClub)
+
+	// club service api for student
+	router.GET("/v1/clubs/sorted-by/update", httpHandler.GetClubsSortByUpdateTime)
+	router.GET("/v1/recruitments/sorted-by/create", httpHandler.GetRecruitmentsSortByCreateTime)
+	router.GET("/v1/clubs/uuid/:club_uuid", httpHandler.GetClubInformWithUUID)
+	router.GET("/v1/clubs", httpHandler.GetClubInformsWithUUIDs)
+	router.GET("/v1/recruitments/uuid/:recruitment_uuid", httpHandler.GetRecruitmentInformWithUUID)
+	router.GET("/v1/clubs/uuid/:club_uuid/recruitment-uuid", httpHandler.GetRecruitmentUUIDWithClubUUID)
+	router.GET("/v1/recruitment-uuids", httpHandler.GetRecruitmentUUIDsWithClubUUIDs)
+	router.GET("/v1/clubs/property/fields", httpHandler.GetAllClubFields)
+	router.GET("/v1/clubs/count", httpHandler.GetTotalCountOfClubs)
+	router.GET("/v1/recruitments/count", httpHandler.GetTotalCountOfCurrentRecruitments)
+	router.GET("/v1/leaders/uuid/:leader_uuid/club-uuid", httpHandler.GetClubUUIDWithLeaderUUID)
+
+	// club service api for club leader
+	router.POST("/v1/clubs/uuid/:club_uuid/members", httpHandler.AddClubMember)
+	router.DELETE("/v1/clubs/uuid/:club_uuid/members/:student_uuid", httpHandler.DeleteClubMember)
+	router.PUT("/v1/clubs/uuid/:club_uuid/leader", httpHandler.ChangeClubLeader)
+	router.PATCH("/v1/clubs/uuid/:club_uuid", httpHandler.ModifyClubInform)
+	router.DELETE("/v1/clubs/uuid/:club_uuid", httpHandler.DeleteClubWithUUID)
+	router.POST("/v1/recruitments", httpHandler.RegisterRecruitment)
+	router.PATCH("/v1/recruitments/uuid/:recruitment_uuid", httpHandler.ModifyRecruitment)
+	router.DELETE("/v1/recruitments/uuid/:recruitment_uuid", httpHandler.DeleteRecruitment)
+
+	log.Fatal(router.Run(":8080"))
 }
