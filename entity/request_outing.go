55f6affa3092d4202a83dd350acaae622f38f2d5
지농 <jinhong0719@naver.com>
2020-12-02 23:48:20 +0900

    [ADD] <entity/request_outing.go> Add GetStudentOutingsRequest struct and method

--- entity/request_outing.go
+++ entity/request_outing.go
@@ -1,1 +1,18 @@
 package entity
+
+import (
+	outingproto "gateway/proto/golang/outing"
+)
+
+// request entity of GET /v1/clubs/paging
+type GetStudentOutingsRequest struct {
+	Start int32  `form:"start"`
+	Count int32  `form:"count"`
+}
+
+func (from GetStudentOutingsRequest) GenerateGRPCRequest() (to *outingproto.GetStudentOutingsRequest) {
+	to = new(outingproto.GetStudentOutingsRequest)
+	to.Start = from.Start
+	to.Count = from.Count
+	return
+}
