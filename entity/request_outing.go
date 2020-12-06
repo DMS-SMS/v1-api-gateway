7f60ad9c8e6dcb21ca9687f10c44eaa1bd2d5369
지농 <jinhong0719@naver.com>
2020-12-06 16:49:51 +0900

    [ADD] <entity/request_outing.go> Add Request entity for CreateOutingRequest

--- entity/request_outing.go
+++ entity/request_outing.go
@@ -4,7 +4,26 @@ import (
 	outingproto "gateway/proto/golang/outing"
 )
 
-// request entity of GET /v1/clubs/paging
+// request entity of POST /v1/outings
+type CreateOutingRequest struct {
+	StartTime int64  `json:"start_time" validate:"required,int_len=10"`
+	EndTime   int64  `json:"end_time" validate:"required,int_len=10"`
+	Place     string `json:"place" validate:"required,max=50"`
+	Reason    string `json:"reason" validate:"required,max=150"`
+	Situation string `json:"situation" validate:"required,values=normal&emergency"`
+}
+
+func (from CreateOutingRequest) GenerateGRPCRequest() (to *outingproto.CreateOutingRequest) {
+	to = new(outingproto.CreateOutingRequest)
+	to.StartTime = from.StartTime
+	to.EndTime = from.EndTime
+	to.Place = from.Place
+	to.Reason = from.Reason
+	to.Situation = from.Situation
+	return
+}
+
+// request entity of GET /v1/students/uuid/:student_uuid/outings
 type GetStudentOutingsRequest struct {
 	Start int32  `form:"start"`
 	Count int32  `form:"count"`
