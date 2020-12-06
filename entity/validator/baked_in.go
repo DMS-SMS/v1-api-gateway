d43b3c15d81a30d18de8f8bfcaf498b66435e8e1
지농 <jinhong0719@naver.com>
2020-12-06 16:22:29 +0900

    [ADD] <entity/validator/baked_in.go> Add isValidValue function for validator

--- entity/validator/baked_in.go
+++ entity/validator/baked_in.go
@@ -79,3 +79,14 @@ func isTime(fl validator.FieldLevel) bool {
 	}
 	return timeRegex.MatchString(fl.Field().String())
 }
+
+func isValidValue(fl validator.FieldLevel) bool {
+	availableValues := strings.Split(fl.Param(), "&")
+	value := fl.Field().String()
+	for _, availableValue := range availableValues {
+		if availableValue == value {
+			return true
+		}
+	}
+	return false
+}
