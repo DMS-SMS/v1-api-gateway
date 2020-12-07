a72b1cc25da4700189b59835e8592c873b1c7a70
지농 <jinhong0719@naver.com>
2020-12-07 23:15:33 +0900

    [ADD] <entity/request_schedule.go> Add request entity for CreateSchedule method

--- entity/request_schedule.go
+++ entity/request_schedule.go
@@ -1,1 +1,20 @@
 package entity
+
+import (
+	scheduleproto "gateway/proto/golang/schedule"
+)
+
+// request entity of POST /v1/outings
+type CreateScheduleRequest struct {
+	StartDate int64  `json:"start_date" validate:"required,int_len=10"`
+	EndDate   int64  `json:"end_date" validate:"required,int_len=10"`
+	Detail    string `json:"detail" validate:"required,len=100"`
+}
+
+func (from CreateScheduleRequest) GenerateGRPCRequest() (to *scheduleproto.CreateScheduleRequest) {
+	to = new(scheduleproto.CreateScheduleRequest)
+	to.StartDate = from.StartDate
+	to.EndDate = from.EndDate
+	to.Detail = from.Detail
+	return
+}
