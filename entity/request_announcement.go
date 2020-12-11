4979c7ab1cef04c712ec1f220e2089b62be24bc3
지농 <jinhong0719@naver.com>
2020-12-11 17:17:19 +0900

    [ADD] <entity/request_announcement.go> Add reqeust entity for CreateAnnouncement

--- entity/request_announcement.go
+++ entity/request_announcement.go
@@ -1,1 +1,24 @@
 package entity
+
+import (
+	announcementproto "gateway/proto/golang/announcement"
+)
+
+// request entity of POST /v1/announcements
+type CreateAnnouncementRequest struct {
+	Type        string `json:"type" validate:"required,values=school&club"`
+	Title       string `json:"title" validate:"required,max=50"`
+	Content     string `json:"content" validate:"required,max=1000"`
+	TargetGrade int32  `json:"target_grade" validate:"int_range=1~3"`
+	TargetGroup int32  `json:"target_group" validate:"int_range=1~4"`
+}
+
+func (from CreateAnnouncementRequest) GenerateGRPCRequest() (to *announcementproto.CreateAnnouncementRequest) {
+	to = new(announcementproto.CreateAnnouncementRequest)
+	to.Type = from.Type
+	to.Title = from.Title
+	to.Content = from.Content
+	to.TargetGrade = from.TargetGrade
+	to.TargetGroup = from.TargetGroup
+	return
+}
