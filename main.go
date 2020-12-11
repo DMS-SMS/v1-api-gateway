5eae64e16ec5c209a104475fa1484f9dc88ffd5d
지농 <jinhong0719@naver.com>
2020-12-11 19:36:56 +0900

    [MODIFY] <main.go> Modify announcement service HTTP API path

--- main.go
+++ main.go
@@ -213,11 +213,11 @@ func main() {
 
 	announcementRouter := router.Group("/", middleware.LogEntrySetter(announcementLogger))
 	announcementRouter.POST("/v1/announcements", httpHandler.CreateAnnouncement)
-	announcementRouter.GET("/v1/announcements/types/{type}", httpHandler.GetAnnouncements)
-	announcementRouter.GET("/v1/announcements/uuid/{announcement_uuid}", httpHandler.GetAnnouncementDetail)
-	announcementRouter.PATCH("/v1/announcements/uuid/{announcement_uuid}", httpHandler.UpdateAnnouncement)
-	announcementRouter.DELETE("/v1/announcements/uuid/{announcement_uuid}", httpHandler.DeleteAnnouncement)
-	announcementRouter.GET("/v1/students/uuid/{student_uuid}/announcement-check", httpHandler.CheckAnnouncement)
+	announcementRouter.GET("/v1/announcements/types/:type", httpHandler.GetAnnouncements)
+	announcementRouter.GET("/v1/announcements/uuid/:announcement_uuid", httpHandler.GetAnnouncementDetail)
+	announcementRouter.PATCH("/v1/announcements/uuid/:announcement_uuid", httpHandler.UpdateAnnouncement)
+	announcementRouter.DELETE("/v1/announcements/uuid/:announcement_uuid", httpHandler.DeleteAnnouncement)
+	announcementRouter.GET("/v1/students/uuid/:student_uuid/announcement-check", httpHandler.CheckAnnouncement)
 
 	log.Fatal(router.Run(":80"))
 }
