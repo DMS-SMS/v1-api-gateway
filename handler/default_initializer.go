89fff105b861d98cb4df8140f0c62a250eeef0ee
지농 <jinhong0719@naver.com>
2020-12-15 17:13:33 +0900

    [ADD] <handler/default_initializer.go> Add NAVER_CLIENT_ID & NAVER_CLIENT_SECRET

--- handler/default_initializer.go
+++ handler/default_initializer.go
@@ -1,1 +1,18 @@
 package handler
+
+import (
+	"log"
+	"os"
+)
+
+var naverClientID string
+var naverClientSecret string
+
+func init() {
+	if naverClientID = os.Getenv("NAVER_CLIENT_ID"); naverClientID == "" {
+		log.Fatal("please set NAVER_CLIENT_ID in environment variable")
+	}
+	if naverClientSecret = os.Getenv("NAVER_CLIENT_SECRET"); naverClientSecret == "" {
+		log.Fatal("please set NAVER_CLIENT_SECRET in environment variable")
+	}
+}
