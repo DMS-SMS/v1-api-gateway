4b7ef40ae3167aff449536fe3df57e31bfbc151b
지농 <jinhong0719@naver.com>
2020-12-15 17:14:41 +0900

    [ADD] <entity/response_open_api.go> Add response entity GetPlaceWithNaverOpenAPI

--- entity/response_open_api.go
+++ entity/response_open_api.go
@@ -1,1 +1,20 @@
 package entity
+
+type GetPlaceWithNaverOpenAPIResponse struct {
+	LastBuildDate string `json:"lastBuildDate"`
+	Total         int    `json:"total"`
+	Start         int    `json:"start"`
+	Display       int    `json:"display"`
+	Items         []struct {
+		Title       string `json:"title"`
+		Link        string `json:"link"`
+		Category    string `json:"category"`
+		Telephone   string `json:"telephone"`
+		Address     string `json:"address"`
+		RoadAddress string `json:"roadAddress"`
+		Mapx        string `json:"mapx"`
+		Mapy        string `json:"mapy"`
+	} `json:"items"`
+	ErrorMessage string `json:"errorMessage"`
+	ErrorCode    string `json:"errorCode"`
+}
