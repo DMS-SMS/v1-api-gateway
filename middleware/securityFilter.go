af3b4b223aad8cb0e281c4b1b8d2a7ae591534cf
지농 <jinhong0719@naver.com>
2020-12-22 21:39:34 +0900

    [ADD] <middleware/securityFilter.go> Add filterSecurity method

--- middleware/securityFilter.go
+++ middleware/securityFilter.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/mervick/aes-everywhere/go/aes256"
+	"net/http"
 	"regexp"
 )
 
@@ -10,4 +13,40 @@ type securityFilter struct {
 	filteredSecurity  map[string]bool
 	onceUsedSecurity  map[string]bool
 	basePlainTemplate *regexp.Regexp
+}
+
+func (s *securityFilter) filterSecurity(c *gin.Context) {
+	respFor407 := struct {
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+	}{
+		Status: http.StatusProxyAuthRequired,
+		Message: "please send the request through the proxy",
+	}
+
+	security := c.GetHeader("Request-Filter")
+	if security == "" {
+		c.AbortWithStatusJSON(http.StatusProxyAuthRequired, respFor407)
+		return
+	}
+
+	if s.filteredSecurity[security] {
+		c.AbortWithStatusJSON(http.StatusProxyAuthRequired, respFor407)
+		return
+	}
+
+	if s.onceUsedSecurity[security] {
+		c.AbortWithStatusJSON(http.StatusProxyAuthRequired, respFor407)
+		return
+	}
+
+	decrypted := aes256.Decrypt(security, s.passPhrase)
+	if decrypted == "" || !s.basePlainTemplate.MatchString(decrypted) {
+		s.filteredSecurity[security] = true
+		c.AbortWithStatusJSON(http.StatusProxyAuthRequired, respFor407)
+		return
+	}
+
+	s.onceUsedSecurity[security] = true
+	c.Next()
 }
