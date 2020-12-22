4461f71c7581bfd4514ae2021f9c3f003b095145
지농 <jinhong0719@naver.com>
2020-12-22 21:38:45 +0900

    [ADD] <middleware/securityFilter.go> Add onceUsedSecurity & basePlainTemplate

--- middleware/securityFilter.go
+++ middleware/securityFilter.go
@@ -1,7 +1,13 @@
 package middleware
 
+import (
+	"regexp"
+)
+
 type securityFilter struct {
-	basePlain      string
-	passPhrase     string
-	filteredSecure map[string]bool
+	basePlain         string
+	passPhrase        string
+	filteredSecurity  map[string]bool
+	onceUsedSecurity  map[string]bool
+	basePlainTemplate *regexp.Regexp
 }
