92494ed3f15fd7810f7e4fe4fd732fda2bea2bca
지농 <jinhong0719@naver.com>
2021-01-19 16:09:21 +0900

    [ADD] <handler/default_event_publish.go> Add PublishConsulChangeEvent method

--- handler/default_event_publish.go
+++ handler/default_event_publish.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *_default) PublishConsulChangeEvent (c *gin.Context) {
+	respFor407 := struct {
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+	}{
+		Status: http.StatusProxyAuthRequired,
+		Message: "please send the request through the proxy",
+	}
+
+	switch true {
+	case c.GetHeader(consulSecretHeader) != "":
+	default:
+		c.AbortWithStatusJSON(http.StatusProxyAuthRequired, respFor407)
+		return
+	}
+}
