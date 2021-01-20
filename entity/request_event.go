f20627f793a02bf7041e45ef1fbbd66e1e917c84
지농 <jinhong0719@naver.com>
2021-01-20 12:26:29 +0900

    [ADD] <entity/request_event.go> Add Node struct in request entity

--- entity/request_event.go
+++ entity/request_event.go
@@ -1,1 +1,10 @@
 package entity
+
+// request entity for GET /v1/clubs/uuid/:club_uuid/members
+type PublishConsulChangeEventRequest struct {
+	Node struct {
+		ID      string `json:"ID"`
+		Node    string `json:"Node"`
+		Address string `json:"Address"`
+	} `json:"Node"`
+}
