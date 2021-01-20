c9ef1964014180b559d85a43df55c46b1f889b1d
지농 <jinhong0719@naver.com>
2021-01-20 10:21:35 +0900

    [MODIFY] <handler/initilizer.go> Change env variable name to CONSUL_INDEX_HEADER

--- handler/initializer.go
+++ handler/initializer.go
@@ -7,7 +7,7 @@ import (
 
 var naverClientID string
 var naverClientSecret string
-var consulSecretHeader string
+var consulIndexHeader string
 
 func init() {
 	if naverClientID = os.Getenv("NAVER_CLIENT_ID"); naverClientID == "" {
@@ -16,8 +16,8 @@ func init() {
 	if naverClientSecret = os.Getenv("NAVER_CLIENT_SECRET"); naverClientSecret == "" {
 		log.Fatal("please set NAVER_CLIENT_SECRET in environment variable")
 	}
-	if consulSecretHeader = os.Getenv("CONSUL_SECRET_HEADER"); consulSecretHeader == "" {
-		log.Fatal("please set CONSUL_SECRET_HEADER in environment variable")
+	if consulIndexHeader = os.Getenv("CONSUL_INDEX_HEADER"); consulIndexHeader == "" {
+		log.Fatal("please set CONSUL_INDEX_HEADER in environment variable")
 	}
 }
 
