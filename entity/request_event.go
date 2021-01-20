5c9c2207e83f31906d6e6812569d4ec13734494e
지농 <jinhong0719@naver.com>
2021-01-20 12:26:51 +0900

    [ADD] <entity/request_event.go> Add Service struct in reqeust entity

--- entity/request_event.go
+++ entity/request_event.go
@@ -7,4 +7,11 @@ type PublishConsulChangeEventRequest struct {
 		Node    string `json:"Node"`
 		Address string `json:"Address"`
 	} `json:"Node"`
+
+	Service struct {
+		ID      string `json:"ID"`
+		Service string `json:"Service"`
+		Port    int    `json:"Port"`
+		Address string `json:"Address"`
+	} `json:"Service"`
 }
