1792eb130f7a6da072cec41a2ab00fb4ffb278b7
지농 <jinhong0719@naver.com>
2021-01-21 11:11:43 +0900

    [CREATE] <tool/environment/get_env.go> Create to declare func about getting env

---
+++ tool/environment/get_env.go
@@ -0,0 +1,5 @@
+// Add package in v.1.0.2
+// this package is used for utility to environment variables for getting, setting, etc...
+// get_env.go is file to about getting environment
+
+package environment
