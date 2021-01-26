5c4c25d093ace7db5b87724ce5fcf21b51758678
지농 <jinhong0719@naver.com>
2021-01-26 12:04:06 +0900

    [ADD] <router/custom_router.go> Add customRouter struct & annotation

--- router/custom_router.go
+++ router/custom_router.go
@@ -3,3 +3,12 @@
 // custom_router.go is file that declare custom router struct & initializer
 
 package router
+
+import "github.com/gin-gonic/gin"
+
+// customRouter basically embedding *gin.Engine, and declare to override additional function in basic router
+// Additional function is run closure after & before server start or end, etc ...
+type customRouter struct {
+	*gin.Engine
+	beforeStart []func()
+}
