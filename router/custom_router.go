77c638eb888607649881d25a496ebee90efe82f5
지농 <jinhong0719@naver.com>
2021-01-26 11:33:58 +0900

    [CREATE] <router/custom_router.go> Create router package & add annotation

---
+++ router/custom_router.go
@@ -0,0 +1,5 @@
+// add package in v.1.0.2
+// this package is for declaring custom router, overriding *gin.Engine
+// custom_router.go is file that declare custom router struct & initializer
+
+package router
