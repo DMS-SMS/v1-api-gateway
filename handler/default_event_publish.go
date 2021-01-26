93669c0cffddb7208139a82eef24bbd0759a7d93
지농 <jinhong0719@naver.com>
2021-01-26 12:33:51 +0900

    [MODIFY] <handler/default_event_publish.go> Delete TODO annotation

--- handler/default_event_publish.go
+++ handler/default_event_publish.go
@@ -33,11 +33,6 @@ func (h *_default) PublishConsulChangeEvent (c *gin.Context) {
 		return
 	}
 
-	// 이벤트 발생
-	// 모든 서비스 조회 새로고침 (해당 서비스 포함)
-	// 해당 서비스에 대한 연결을 새로 맺어야 한다는 뜻이니까 health checker도 받아서 ping 보냄
-	// 참고로 해당 서비스가 새로 시작될 때도 이벤트 발생 필요. (없었을 수도 있으니)
-
 	// 현재는 checks를 따로 처리하진 않음
 	if c.GetHeader("Type") == "checks" {
 		c.Status(http.StatusOK)
