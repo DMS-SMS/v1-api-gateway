d6dd47d8c6d2ace26a87454c4f864089a1132d49
지농 <jinhong0719@naver.com>
2021-01-28 20:08:36 +0900

    [ADD] <consul/kvEntity.go> Add entity struct for redis connection config KV

--- consul/kvEntity.go
+++ consul/kvEntity.go
@@ -2,3 +2,10 @@
 // kvEntity.go is file that declare entities for value in KV
 
 package consul
+
+// entity about redis connection config KV
+type RedisConfigKV struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
+	DB   int    `json:"DB"`
+}
