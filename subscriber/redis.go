ff78a0d53856225fac6030eafe3ecd9849c0a581
지농 <jinhong0719@naver.com>
2021-01-29 18:26:51 +0900

    [ADD] <subscriber/redis.go> Add redis message listen & handler return method

--- subscriber/redis.go
+++ subscriber/redis.go
@@ -5,8 +5,27 @@
 package subscriber
 
 import (
+	"context"
 	"github.com/go-redis/redis/v8"
+	log "github.com/micro/go-micro/v2/logger"
 )
 
 // function signature type for redis message handler
 type redisMsgHandler func(*redis.Message) error
+
+// function that returns closure listening redis message & handling with function receive from parameter
+func RedisListener(topic string, handler redisMsgHandler, chlSize int) func() {
+	pubsub := redisCli.Subscribe(context.Background(), topic)
+	pubChl := pubsub.ChannelSize(chlSize)
+
+	return func() {
+		for {
+			pubMsg := <- pubChl
+			go func(msg *redis.Message) {
+				if err := handler(msg); err != nil {
+					log.Errorf("some error occurs while handling redis message, topic: %s, msg: %s err: %v", topic, pubMsg.String(), err)
+				}
+			}(pubMsg)
+		}
+	}
+}
