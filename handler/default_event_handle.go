6b373ac4826774c5adb6ee1eebe6a37664fdee21
지농 <jinhong0719@naver.com>
2021-01-29 23:39:16 +0900

    [ADD] <handler/default_event_handler.go> Add DeleteWithKeyPattern method

--- handler/default_event_handle.go
+++ handler/default_event_handle.go
@@ -4,12 +4,34 @@
 package handler
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/go-redis/redis/v8"
 	log "github.com/micro/go-micro/v2/logger"
 )
+
+var ctx = context.Background()
 
 func (h *_default) ChangeConsulNodes(message *sqs.Message) (err error) {
 	err = h.consulAgent.ChangeAllServiceNodes()
 	log.Infof("change all service nodes!, err: %v", err)
+	return
+}
+
+func (h *_default) DeleteWithKeyPattern(msg *redis.Message) (err error) {
+	keys, err := h.redisClient.Keys(ctx, msg.Payload).Result()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("unable to execute redis KEYS cmd, err: %v", err))
+	}
+
+	for _, key := range keys {
+		if _, err := h.redisClient.Del(ctx, key).Result(); err != nil {
+			err = errors.New(fmt.Sprintf("unable to execute redis DEL cmd, key: %s, err: %v", key, err))
+		}
+	}
+
+	log.Infof("delete all redis key with pattern!, msg: %s, key num: %d", msg.String(), len(keys))
 	return
 }
