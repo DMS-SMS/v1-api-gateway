faa1795bed9eeeaa2b58379042001edbb67281b4
지농 <jinhong0719@naver.com>
2021-01-29 16:11:02 +0900

    [CREATE] <subscriber/redis.go> Create file to declare redis queue listener

---
+++ subscriber/redis.go
@@ -0,0 +1,5 @@
+// create file in v.1.0.3
+// redis.go is file that declare closure return method about listening redis message queue
+// you can also use this by registry with method in subscriber
+
+package subscriber
