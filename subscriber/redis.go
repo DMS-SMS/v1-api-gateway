ee928527db025506ce01d775c509772cf611ff46
지농 <jinhong0719@naver.com>
2021-01-29 18:18:58 +0900

    [ADD] <subscriber/redis.go> Add Reids message handler function signature

--- subscriber/redis.go
+++ subscriber/redis.go
@@ -3,3 +3,10 @@
 // you can also use this by registry with method in subscriber
 
 package subscriber
+
+import (
+	"github.com/go-redis/redis/v8"
+)
+
+// function signature type for redis message handler
+type redisMsgHandler func(*redis.Message) error
