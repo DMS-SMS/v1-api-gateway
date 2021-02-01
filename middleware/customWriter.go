a773d2306b9bd3f563a90425329676a8cc690abd
지농 <jinhong0719@naver.com>
2021-02-01 18:26:12 +0900

    [ADD] <middleware/customWriter.go> Add overriding Write mehod of ginHResponseWriter

--- middleware/customWriter.go
+++ middleware/customWriter.go
@@ -4,10 +4,25 @@
 package middleware
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 type ginHResponseWriter struct {
 	gin.ResponseWriter
 	json gin.H
+}
+
+// save response(value of gin.H type) in field of ginHResponseWriter
+func (w *ginHResponseWriter) Write(b []byte) (int, error) {
+	resp := gin.H{}
+	if err := json.Unmarshal(b, &resp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("error occurs while unmarshal response json to obj in custom response writer, err: %v", err)
+	}
+
+	w.json = resp
+	return w.ResponseWriter.Write(b)
 }
