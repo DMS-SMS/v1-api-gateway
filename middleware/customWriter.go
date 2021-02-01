1f2928979ee70420af30e8b59f6cb0f9fac51b04
지농 <jinhong0719@naver.com>
2021-02-01 18:24:19 +0900

    [ADD] <middleware/customWriter.go> Add ginHResponseWriter embedding gin rw

--- middleware/customWriter.go
+++ middleware/customWriter.go
@@ -2,3 +2,12 @@
 // customWriter.go is file that declare custom writer overriding default writer in gin context
 
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type ginHResponseWriter struct {
+	gin.ResponseWriter
+	json gin.H
+}
