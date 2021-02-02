950c14efe0eb8f1469ef7ac9aeb2db3bd7d56c84
지농 <jinhong0719@naver.com>
2021-02-02 10:07:52 +0900

    [ADD] <middleware/tracerSpanStarter.go> Add TracerSpanStarter struct & initalizer

--- middleware/tracerSpanStarter.go
+++ middleware/tracerSpanStarter.go
@@ -1,4 +1,19 @@
 // add file in v.1.0.3
-// tracerSpanStarter.go is file that gin middleware for start top span of tracer
+// tracerSpanStarter.go is file that gin middleware to start, log & end top span of tracer
 
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+)
+
+type tracerSpanStarter struct {
+	tracer opentracing.Tracer
+}
+
+func TracerSpanStarter(t opentracing.Tracer) gin.HandlerFunc {
+	return (&tracerSpanStarter{
+		tracer: t,
+	}).StartTracerSpan
+}
