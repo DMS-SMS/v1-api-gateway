54e318fc2853704a72edad7bbf6d2353b3573856
지농 <jinhong0719@naver.com>
2021-02-03 10:19:49 +0900

    [RENAME] <router/custom_group.go> Change file name from custom_method.go

---
+++ router/custom_group.go
@@ -0,0 +1,4 @@
+// add file in v.1.0.3
+// custom_group.go is file that declare method, overriding method of customRouterGroup
+
+package router
--- router/custom_method.go
+++
@@ -1,4 +0,0 @@
-// add file in v.1.0.3
-// custom_method.go is file that declare method, not overriding, of custom router struct
-
-package router
