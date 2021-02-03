1e717ed0a83462fe91dcb9928fa45891477e9a96
지농 <jinhong0719@naver.com>
2021-02-03 10:19:18 +0900

    [RENAME] <router/custom_router.go> Change file name from custom_override.go

--- router/custom_router.go
+++ router/custom_router.go
@@ -1,5 +1,5 @@
 // add package in v.1.0.2
-// custom_override.go is file that declare overriding method of customRouter
+// custom_router.go is file that declare method, overriding method of customRouter
 
 package router
 
