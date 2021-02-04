d57b30cb668f6cb843414dee2637a719d2340ad2
지농 <jinhong0719@naver.com>
2021-02-04 10:29:50 +0900

    [ADD] <router/custom_group.go> Add CustomGroup method in customRouterGroup

--- router/custom_group.go
+++ router/custom_group.go
@@ -2,3 +2,14 @@
 // custom_group.go is file that declare method, overriding method of customRouterGroup
 
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// method that return custom router group having method declared in this file
+func (g *customRouterGroup) CustomGroup(relativePath string, handlers ...gin.HandlerFunc) *customRouterGroup {
+	return &customRouterGroup{
+		RouterGroup: g.RouterGroup.Group(relativePath, handlers...),
+	}
+}
