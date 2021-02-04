32732f856afffdeb986a02986322ea000c2cb0a9
지농 <jinhong0719@naver.com>
2021-02-04 16:24:49 +0900

    [ADD] <middleware/request_validator.go> Add requestValidator struct & global var

--- middleware/request_validator.go
+++ middleware/request_validator.go
@@ -2,3 +2,14 @@
 // request_validator.go is file that declare closure bind request body to golang struct & validate request
 
 package middleware
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+// create all closure from this global struct method
+var globalValidator *requestValidator
+
+type requestValidator struct {
+	validator *validator.Validate
+}
