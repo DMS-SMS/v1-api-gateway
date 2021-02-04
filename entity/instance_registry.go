73bbb8948e081c79da3ab59febb17a84ec128a90
지농 <jinhong0719@naver.com>
2021-02-04 17:51:40 +0900

    [CREATE] <entity/instance_registry.go> Create file to declare instance registry

---
+++ entity/instance_registry.go
@@ -0,0 +1,5 @@
+// add file in v.1.0.3
+// instance_registry.go is file that save all instance of each request entity in map
+// this registry is used to declare new instance with string in middleware.RequestValidator
+
+package entity
