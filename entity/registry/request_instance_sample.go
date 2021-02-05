1e9d57661c6bebdea49adeb12edc9cacd65d86db
지농 <jinhong0719@naver.com>
2021-02-05 12:44:29 +0900

    [ADD] <entity/registry/request_instance_sample.go> Add all request entity sample

--- entity/registry/request_instance_sample.go
+++ entity/registry/request_instance_sample.go
@@ -2,3 +2,59 @@
 // request_instance_sample.go is file that gather instance sample that set in request instance registry
 
 package registry
+
+import "gateway/entity"
+
+var requestSamples = map[string]interface{}{
+	// in "entity/request_announcement.go"
+	"CreateAnnouncementRequest": entity.CreateAnnouncementRequest{},
+	"GetAnnouncementsRequest": entity.GetAnnouncementsRequest{},
+	"UpdateAnnouncementRequest": entity.UpdateAnnouncementRequest{},
+	"SearchAnnouncementsRequest": entity.SearchAnnouncementsRequest{},
+	"GetMyAnnouncementsRequest": entity.GetMyAnnouncementsRequest{},
+
+	// in "entity/request_auth.go"
+	"CreateNewStudentRequest": entity.CreateNewStudentRequest{},
+	"CreateNewTeacherRequest": entity.CreateNewTeacherRequest{},
+	"CreateNewParentRequest": entity.CreateNewParentRequest{},
+	"LoginAdminAuthRequest": entity.LoginAdminAuthRequest{},
+	"LoginStudentAuthRequest": entity.LoginStudentAuthRequest{},
+	"ChangeStudentPWRequest": entity.ChangeStudentPWRequest{},
+	"GetStudentUUIDsWithInformRequest": entity.GetStudentUUIDsWithInformRequest{},
+	"GetStudentInformsWithUUIDsRequest": entity.GetStudentInformsWithUUIDsRequest{},
+	"LoginTeacherAuthRequest": entity.LoginTeacherAuthRequest{},
+	"ChangeTeacherPWRequest": entity.ChangeTeacherPWRequest{},
+	"GetTeacherUUIDsWithInformRequest": entity.GetTeacherUUIDsWithInformRequest{},
+	"LoginParentAuthRequest": entity.LoginParentAuthRequest{},
+	"ChangeParentPWRequest": entity.ChangeParentPWRequest{},
+	"GetParentUUIDsWithInformRequest": entity.GetParentUUIDsWithInformRequest{},
+
+	// in "entity/request_club.go"
+	"CreateNewClubRequest": entity.CreateNewClubRequest{},
+	"GetClubsSortByUpdateTimeRequest": entity.GetClubsSortByUpdateTimeRequest{},
+	"GetRecruitmentsSortByCreateTimeRequest": entity.GetRecruitmentsSortByCreateTimeRequest{},
+	"GetClubInformsWithUUIDsRequest": entity.GetClubInformsWithUUIDsRequest{},
+	"GetRecruitmentUUIDsWithClubUUIDsRequest": entity.GetRecruitmentUUIDsWithClubUUIDsRequest{},
+	"AddClubMemberRequest": entity.AddClubMemberRequest{},
+	"ChangeClubLeaderRequest": entity.ChangeClubLeaderRequest{},
+	"ModifyClubInformRequest": entity.ModifyClubInformRequest{},
+	"RegisterRecruitmentRequest": entity.RegisterRecruitmentRequest{},
+	"ModifyRecruitmentRequest": entity.ModifyRecruitmentRequest{},
+
+	// in "entity/request_event.go"
+	"PublishConsulChangeEventRequest": entity.PublishConsulChangeEventRequest{},
+
+	// in "entity/request_opne_api.go"
+	"GetPlaceWithNaverOpenAPIRequest": entity.GetPlaceWithNaverOpenAPIRequest{},
+
+	// in "entity/request_outing.go"
+	"CreateOutingRequest": entity.CreateOutingRequest{},
+	"GetStudentOutingsRequest": entity.GetStudentOutingsRequest{},
+	"GetOutingWithFilterRequest": entity.GetOutingWithFilterRequest{},
+
+	// in "entity/request_schedule.go"
+	"CreateScheduleRequest": entity.CreateScheduleRequest{},
+	"GetScheduleRequest": entity.GetScheduleRequest{},
+	"GetTimeTableRequest": entity.GetTimeTableRequest{},
+	"UpdateScheduleRequest": entity.UpdateScheduleRequest{},
+}
