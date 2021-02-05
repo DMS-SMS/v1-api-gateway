ed80fd6b47e94e3ae6dd0655edb23d7288f90333
지농 <jinhong0719@naver.com>
2021-02-05 12:06:03 +0900

    [CREATE] <entity/registry/initalizer.go> Create file that declare initializer

---
+++ entity/registry/initializer.go
@@ -0,0 +1,5 @@
+// add package in v.1.0.3
+// registry package is used to get entity instance with string
+// initializer.go is file that scan all struct about request entity in files in entity package
+
+package registry
