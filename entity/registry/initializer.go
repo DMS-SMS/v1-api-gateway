ed11f5e595fa2b87d86ffb74f30f8e8f4cc7edda
지농 <jinhong0719@naver.com>
2021-02-05 12:22:01 +0900

    [ADD] <entity/registry/initalizer.go> Add init funciton & loading request entity files

--- entity/registry/initializer.go
+++ entity/registry/initializer.go
@@ -3,3 +3,23 @@
 // initializer.go is file that scan all struct about request entity in files in entity package
 
 package registry
+
+import (
+	"io/ioutil"
+	"log"
+	"regexp"
+)
+
+func init() {
+	entityDir := "/usr/share/gateway/entity/"
+	files, err := ioutil.ReadDir(entityDir)
+	if err != nil {
+		log.Fatalf("unable to read dir, dir: %s\n", entityDir)
+	}
+	var targetFiles []string
+	for _, f := range files {
+		if regexp.MustCompile("^request_.*.go$").MatchString(f.Name()) {
+			targetFiles = append(targetFiles, entityDir + f.Name())
+		}
+	}
+}
