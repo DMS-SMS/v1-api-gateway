c704949b975f7a461b42487a20a4abee71d86d51
지농 <jinhong0719@naver.com>
2021-02-08 12:27:18 +0900

    [ADD] <middleware/redis_handler.go> Add declaring redisHandler struct & initalizer

--- middleware/redis_handler.go
+++ middleware/redis_handler.go
@@ -2,3 +2,20 @@
 // redis_handler.go is file that declare various handler about redis etc searching, publishing, etc ...
 
 package middleware
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/opentracing/opentracing-go"
+)
+
+type redisHandler struct {
+	client *redis.Client
+	tracer opentracing.Tracer
+}
+
+func RedisHandler(cli *redis.Client, tracer opentracing.Tracer) *redisHandler {
+	return &redisHandler{
+		client: cli,
+		tracer: tracer,
+	}
+}
