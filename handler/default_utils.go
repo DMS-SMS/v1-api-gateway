926297c62f40b15a3d9276663928de01603cff0e
지농 <jinhong0719@naver.com>
2021-02-09 15:46:27 +0900

    [MODIFY] <handler/default_utils.go> Delete checkIfValidRequest method

--- handler/default_utils.go
+++ handler/default_utils.go
@@ -3,13 +3,11 @@ package handler
 import (
 	"fmt"
 	consulagent "gateway/consul/agent"
-	"gateway/entity"
 	jwtutil "gateway/tool/jwt"
 	code "gateway/utils/code/golang"
 	respcode "gateway/utils/code/golang"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"net/http"
 	"strings"
 )
@@ -65,67 +63,6 @@ func (_ *_default) checkIfAuthenticated(c *gin.Context) (ok bool, claims jwtutil
 		msg = fmt.Sprintf("%s is an unacceptable authentication method", authType)
 		return
 	}
-}
-
-func (h *_default) checkIfValidRequest(c *gin.Context, bindReq interface{}) (ok bool, code int, msg string) {
-	switch bindReq.(type) {
-	case *entity.GetScheduleRequest, *entity.GetTimeTableRequest:
-		if err := c.ShouldBindUri(bindReq); err != nil {
-			ok = false
-			code = respcode.FailToBindRequestToStruct
-			msg = fmt.Sprintf("failed to bind uri in request into golang struct, err: %v", err)
-			return
-		}
-	case *entity.GetClubsSortByUpdateTimeRequest, *entity.GetRecruitmentsSortByCreateTimeRequest, *entity.GetStudentOutingsRequest,
-		 *entity.GetOutingWithFilterRequest, *entity.GetAnnouncementsRequest, *entity.GetPlaceWithNaverOpenAPIRequest,
-		 *entity.GetStudentUUIDsWithInformRequest, *entity.GetTeacherUUIDsWithInformRequest, *entity.GetParentUUIDsWithInformRequest:
-		if err := c.ShouldBindQuery(bindReq); err != nil {
-			ok = false
-			code = respcode.FailToBindRequestToStruct
-			msg = fmt.Sprintf("failed to bind query parameter in request into golang struct, err: %v", err)
-			return
-		}
-	default:
-		switch c.ContentType() {
-		case "multipart/form-data":
-			if err := c.ShouldBindWith(bindReq, binding.FormMultipart); err != nil {
-				ok = false
-				code = respcode.FailToBindRequestToStruct
-				msg = fmt.Sprintf("failed to bind multipart request into golang struct, err: %v", err)
-				return
-			}
-		case "application/json":
-			if err := c.ShouldBindJSON(bindReq); err != nil {
-				ok = false
-				code = respcode.FailToBindRequestToStruct
-				msg = fmt.Sprintf("failed to bind json request into golang struct, err: %v", err)
-				return
-			}
-		case "":
-			if err := c.ShouldBindWith(bindReq, binding.Form); err != nil {
-				ok = false
-				code = respcode.FailToBindRequestToStruct
-				msg = fmt.Sprintf("failed to bind request into golang struct, err: %v", err)
-				return
-			}
-			break
-		default:
-			ok = false
-			code = respcode.UnsupportedContentType
-			msg = fmt.Sprintf("%s is an unsupported content type", c.ContentType())
-			return
-		}
-	}
-
-	if err := h.validate.Struct(bindReq); err != nil {
-		ok = false
-		code = respcode.IntegrityInvalidRequest
-		msg = fmt.Sprintf("request is not valid for integrity constraints, err: %v", err)
-		return
-	}
-
-	ok = true
-	return
 }
 
 // this method is to get status & code & msg value from consul get node error
