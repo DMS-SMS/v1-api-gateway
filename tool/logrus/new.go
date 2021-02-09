6a7d17968e82ded35160518b3b162b4a61ff9044
지농 <jinhong0719@naver.com>
2021-02-09 16:43:33 +0900

    [ADD] <tool/logrus/new.go> Add declaring noneWriter & overriding Write method

--- tool/logrus/new.go
+++ tool/logrus/new.go
@@ -2,3 +2,15 @@
 // this package is used to separate generating logrus.Logger from main
 
 package logrus
+
+import (
+	"io"
+)
+
+type noneWriter struct {
+	io.Writer
+}
+
+func (n noneWriter) Write(p []byte) (_ int, _ error) {
+	return
+}
