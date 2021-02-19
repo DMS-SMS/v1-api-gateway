5479856eae253377011a9bc2290efd7d28b17675
지농 <jinhong0719@naver.com>
2021-02-19 17:52:07 +0900

    [ADD] <handler/default_xlsx_handle.go> Add AddUnsignedStudentsFromExcel method

--- handler/default_xlsx_handle.go
+++ handler/default_xlsx_handle.go
@@ -4,7 +4,18 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"gateway/entity"
+	jwtutil "gateway/tool/jwt"
+	code "gateway/utils/code/golang"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
+	"net/http"
 	"regexp"
+	"strings"
 )
 
 var (
@@ -13,3 +24,25 @@ var (
 	phoneNumberRegex = regexp.MustCompile("^\"\\d{2,3}[-_.]?\\d{3,4}[-_.]?\\d{4}\"$")
 	blankRegex = regexp.MustCompile("(^[ ]+$)|(^$)")
 )
+
+func (h *_default) AddUnsignedStudentsFromExcel(c *gin.Context) {
+	reqID := c.GetHeader("X-Request-Id")
+
+	// get top span from middleware
+	inAdvanceTopSpan, _ := c.Get("TopSpan")
+	topSpan, _ := inAdvanceTopSpan.(opentracing.Span)
+
+	// get log entry from middleware
+	inAdvanceEntry, _ := c.Get("RequestLogEntry")
+	entry, _ := inAdvanceEntry.(*logrus.Entry)
+
+	// get token claim from middleware
+	inAdvanceClaims, _ := c.Get("Claims")
+	uuidClaims, _ := inAdvanceClaims.(jwtutil.UUIDClaims)
+	entry = entry.WithField("user_uuid", uuidClaims.UUID)
+
+	// get bound request entry from middleware
+	inAdvanceReq, _ := c.Get("Request")
+	receivedReq, _ := inAdvanceReq.(*entity.AddUnsignedStudentsFromExcelRequest)
+	reqBytes, _ := json.Marshal(receivedReq)
+}
