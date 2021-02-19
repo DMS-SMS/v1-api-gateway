0fd46e6ac4e635d8ebfad05253d211a609a16d54
지농 <jinhong0719@naver.com>
2021-02-19 17:55:34 +0900

    [ADD] <handler/default_xlsx_handle.go> Add parsing student inform from excel

--- handler/default_xlsx_handle.go
+++ handler/default_xlsx_handle.go
@@ -86,4 +86,90 @@ func (h *_default) AddUnsignedStudentsFromExcel(c *gin.Context) {
 		return
 	}
 
+	status, _code, msg := 0, 0, ""
+	defer func() {
+		c.JSON(status, gin.H{"status": status, "code": _code, "message": msg})
+		entry.WithFields(logrus.Fields{"status": status, "code": _code, "message": msg, "request": string(reqBytes)})
+		if status == http.StatusInternalServerError {
+			entry.Error()
+		} else {
+			entry.Info()
+		}
+		return
+	}()
+
+	// 학년, 반, 번호, 이름, (+ 전화번호)
+	for _, sheet := range excel.GetSheetMap() {
+		rows, err := excel.GetRows(sheet)
+		if err != nil {
+			status = http.StatusBadRequest
+			msg = err.Error()
+			return
+		}
+
+		attrs := rows[0]
+		var studentNumberIndex, nameIndex, phoneNumberIndex int
+		var studentNumberExist, nameExist, phoneNumberExist bool
+
+		// 속성 별 인덱스 조회
+		for i, attr := range attrs {
+			switch attr {
+			case "학번", "student_number":
+				studentNumberIndex = i
+				studentNumberExist = true
+			case "성명", "이름", "name":
+				nameIndex = i
+				nameExist = true
+			case "전화번호", "전화 번호", "phone_number", "전화":
+				phoneNumberIndex = i
+				phoneNumberExist = true
+			default:
+				continue
+			}
+		}
+
+		// 존재 X 속성 존재 시, 반환
+		switch false {
+		case studentNumberExist, nameExist, phoneNumberExist:
+			status = http.StatusBadRequest
+			msg = fmt.Sprintf("각각 학번, 성명, 전화번호와 관련된 속성이 모두 존재해야합니다, sheet: %s", sheet)
+			return
+		}
+
+		// 최대 인덱스 값 구함
+		var maxIndex int
+		for i, e := range []int{studentNumberIndex, nameIndex, phoneNumberIndex} {
+			if i==0 || e > maxIndex {
+				maxIndex = e
+			}
+		}
+
+		rowValues := rows[1:]
+		for _, rowValue := range rowValues {
+			if len(rowValue) - 1 < maxIndex {
+				continue
+			}
+
+			studentNumber := rowValue[studentNumberIndex]
+			name := rowValue[nameIndex]
+			phoneNumber := rowValue[phoneNumberIndex]
+
+			// 공백 값 -> continue
+			switch true {
+			case blankRegex.MatchString(studentNumber), blankRegex.MatchString(name), blankRegex.MatchString(phoneNumber):
+				continue
+			}
+
+			// 옳바르지 않은 형식의 데이터 존재 -> 400 반환
+			switch false {
+			case studentNumberRegex.MatchString(studentNumber), nameRegex.MatchString(name), phoneNumberRegex.MatchString(phoneNumber):
+				status = http.StatusBadRequest
+				msg = fmt.Sprintf("학번, 성명, 전화번호와 관련된 값이 옳바르지 않습니다. (%s %s %s) sheet: %s", studentNumber, name, phoneNumber, sheet)
+				return
+			}
+
+			// 3109 박진홍 "01088378511"
+			// fmt.Println(studentNumber, name, phoneNumber)
+		}
+	}
 }
