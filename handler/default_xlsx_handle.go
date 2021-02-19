ff39323a380392f7fd6b1f38a5d05773da7040db
지농 <jinhong0719@naver.com>
2021-02-19 17:51:06 +0900

    [ADD] <handler/default_xlsx.handle.go> Add regex about excel required value

--- handler/default_xlsx_handle.go
+++ handler/default_xlsx_handle.go
@@ -2,3 +2,14 @@
 // default_xlsx_handle.go is file that declare xlsx handling method
 
 package handler
+
+import (
+	"regexp"
+)
+
+var (
+	studentNumberRegex = regexp.MustCompile("(^[1-3][1-4])([0-1][0-9]$|20|21)")
+	nameRegex = regexp.MustCompile("^[가-힣]+$")
+	phoneNumberRegex = regexp.MustCompile("^\"\\d{2,3}[-_.]?\\d{3,4}[-_.]?\\d{4}\"$")
+	blankRegex = regexp.MustCompile("(^[ ]+$)|(^$)")
+)
