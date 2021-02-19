0d954cee899ca1f2b4fcb00c7b4ff9cc37388706
지농 <jinhong0719@naver.com>
2021-02-19 15:07:54 +0900

    [ADD] <entity/request_xlsx.go> Add AddUnsignedStudentsFromExcelRequest entity

--- entity/request_xlsx.go
+++ entity/request_xlsx.go
@@ -2,3 +2,9 @@
 // request_xlsx.go is file that definition request entity about xlsx handling API
 
 package entity
+
+import "mime/multipart"
+
+type AddUnsignedStudentsFromExcelRequest struct {
+	Excel *multipart.FileHeader `form:"excel" validate:"required"`
+}
