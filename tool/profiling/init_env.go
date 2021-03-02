406d8b53a10661db8a72194a4dff8f45f20711bc
지농 <jinhong0719@naver.com>
2021-03-02 16:01:37 +0900

    [ADD] <tool/profiling/init_env.go> Add setting global aws session in profiling pkg

--- tool/profiling/init_env.go
+++ tool/profiling/init_env.go
@@ -1,12 +1,17 @@
 package profiling
 
 import (
-"log"
+	"gateway/tool/env"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"log"
 "os"
 )
 
 var s3Bucket string
 var version string
+var globalSession *session.Session
 
 func init() {
 	if s3Bucket = os.Getenv("SMS_AWS_BUCKET"); s3Bucket == "" {
@@ -14,6 +19,17 @@ func init() {
 	}
 	if version = os.Getenv("VERSION"); s3Bucket == "" {
 		log.Fatal("please set VERSION in environment variable")
-}
+	}
 
+	awsId := env.GetAndFatalIfNotExits("SMS_AWS_ID")
+	awsKey := env.GetAndFatalIfNotExits("SMS_AWS_KEY")
+	s3Region := env.GetAndFatalIfNotExits("SMS_AWS_REGION")
+	if awsSession, err := session.NewSession(&aws.Config{
+		Region:      aws.String(s3Region),
+		Credentials: credentials.NewStaticCredentials(awsId, awsKey, ""),
+	}); err != nil {
+		log.Fatalf("unable to create aws session, err: %v", err)
+	} else {
+		globalSession = awsSession
+	}
 }
