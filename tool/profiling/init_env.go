e9f082f0e06b1b8e926a502515581c2fdeffd3a3
지농 <jinhong0719@naver.com>
2021-03-02 16:00:00 +0900

    [ADD] <tool/profiling/init_env.go> Add getting VERSION environment variable

--- tool/profiling/init_env.go
+++ tool/profiling/init_env.go
@@ -6,9 +6,14 @@ import (
 )
 
 var s3Bucket string
+var version string
 
 func init() {
 	if s3Bucket = os.Getenv("SMS_AWS_BUCKET"); s3Bucket == "" {
 		log.Fatal("please set SMS_AWS_BUCKET in environment variable")
 	}
+	if version = os.Getenv("VERSION"); s3Bucket == "" {
+		log.Fatal("please set VERSION in environment variable")
+}
+
 }
